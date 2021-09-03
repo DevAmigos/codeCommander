d1e24cec0c03530c2546ca2661f8b8b10b9122ef
darioformoso <88165956+darioformoso@users.noreply.github.com>
2021-09-03 16:08:55 -0400

    ll

--- scripts/script.js
+++ scripts/script.js
@@ -34,11 +34,7 @@ const gameOver = new Audio('../sounds/gameover.mp3');
 const gunFire = new Audio('../sounds/gunfire.mp3');
 const dragonFall= new Audio('../sounds/killFX.mp3')
 // Theme Music
-<<<<<<< HEAD
-const themeMusic = new Audio('./sounds/kirbysong.mp3');
-=======
 const themeMusic = new Audio('../sounds/kirbysong.mp3');
->>>>>>> 03b51231e6d59ed71d7a7cc0dc4e42cdc422b4b5
 
 // Kirby sprite functions
 let x = 20;
